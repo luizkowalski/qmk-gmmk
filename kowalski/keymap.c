cde036e7b66791ec1119138b425744d6173beed4
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2023-08-27 19:41:36 +0200

    Updated API

--- kowalski/keymap.c
+++ kowalski/keymap.c
@@ -1,19 +1,3 @@
-/* Copyright 2021 Glorious, LLC <salman@pcgamingrace.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include QMK_KEYBOARD_H
 
 // Decrease debounce because this keyboard is weird
@@ -57,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     if (host_keyboard_led_state().caps_lock)  {
-        rgb_matrix_set_color(caps_position, hsv_orange);
+        rgb_matrix_set_color(CAPS_POSITION, 255, 127, 80);
     }
     return true;
 }
@@ -74,4 +58,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
 }
-
